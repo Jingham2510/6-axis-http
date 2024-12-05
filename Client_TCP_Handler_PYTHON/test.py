d3636a807627802bf26756a9d50908058b87b368
Jingham2510 <jsphingham@gmail.com>
2024-12-05 10:40:57 +0000

    renamed

--- Client_TCP_Handler_PYTHON/test.py
+++ Client_TCP_Handler_PYTHON/test.py
@@ -3,13 +3,13 @@
 
 #Have to open the RAPID socket first - then connect
 
-import Client_TCP_Handler_PYTHON.tcp_controller as tcp_controller
+import tcp_controller 
 import CMD_LIST
 import cntrl_scripts
 
-six_axis_tcp = tcp_controller.six_axis_tcp_base("192.168.125.1", 55555, CMD_LIST.VALID_CMDS)
+#six_axis_tcp = tcp_controller.six_axis_tcp_base("192.168.125.1", 55555, CMD_LIST.VALID_CMDS)
 
-#six_axis_tcp = tcp_controller.six_axis_tcp_base("127.0.0.1", 55555, CMD_LIST.VALID_CMDS)
+six_axis_tcp = tcp_controller.six_axis_tcp_base("127.0.0.1", 55555, CMD_LIST.VALID_CMDS)
 
 six_axis_tcp.socket_connect()
 
